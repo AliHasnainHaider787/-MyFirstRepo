fb8ac2ceaeb955dba428c60095443a1573b3221f
AliHasnainHaider787 <243662@students.au.edu.pk>
2024-11-25 01:06:58 -0800

    Create Program.cpp
    My first program for sum of  3 number

---
+++ Program.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(int argc,char**argv)
+{
+  int num1,num2,num3;
+  cout<<"enter number 1:"<<endl;
+  cin>>num1;
+    cout<<"enter number 2:"<<endl;
+  cin>>num2;
+    cout<<"enter number 3:"<<endl;
+  cin>>num3;
+  int sum=num1+num2+num3;
+  cout<<"sum is"<<sum<<endl;
+
+  return 0;
+}
