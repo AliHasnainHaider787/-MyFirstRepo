94d6d545b66762e759b79ccce15b77963bcc310f
AliHasnainHaider787 <243662@students.au.edu.pk>
2024-11-25 01:26:39 -0800

    Update Program.cpp
    adding comment to code

--- Program.cpp
+++ Program.cpp
@@ -2,14 +2,18 @@
 using namespace std;
 int main(int argc,char**argv)
 {
+  //variables
   int num1,num2,num3;
+  // get all input
   cout<<"enter number 1:"<<endl;
   cin>>num1;
     cout<<"enter number 2:"<<endl;
   cin>>num2;
     cout<<"enter number 3:"<<endl;
   cin>>num3;
+  //sum logic
   int sum=num1+num2+num3;
+  //output result
   cout<<"sum is"<<sum<<endl;
 
   return 0;
